0c02402fe72aef336228e66f49d8bcf2ef8a00f1
Fengning Luo <freddy.luo@mail.utoronto.ca>
2024-02-08 15:22:39 -0500

    ✅ 35/35 test passed

--- hw/hw3/CSC2125_HW_TicketMarketplace/CSC2125_HW_TicketMarketplace-main/contracts/TicketMarketplace.sol
+++ hw/hw3/CSC2125_HW_TicketMarketplace/CSC2125_HW_TicketMarketplace-main/contracts/TicketMarketplace.sol
@@ -57,7 +57,7 @@ contract TicketMarketplace is ITicketMarketplace {
         events[eventId] = Event(0, maxTickets, pricePerTicket, pricePerTicketERC20);
         eventId += 1;
 
-        emit EventCreated(eventId, maxTickets, pricePerTicket, pricePerTicketERC20);
+        emit EventCreated(eventId - 1, maxTickets, pricePerTicket, pricePerTicketERC20);
     }
     
     function setMaxTicketsForEvent(uint128 eventId, uint128 newMaxTickets) external override {
@@ -122,10 +122,49 @@ contract TicketMarketplace is ITicketMarketplace {
             revert("We don't have that many tickets left to sell!");
         }
 
-        // TODO: mint tickets
+        // Mint the tickets
+        for (uint128 i = 0; i < ticketCount; i++) {
+            // get nftId of the ticket
+            // Recall that ticket has unique ID of 256 bits
+            // eventID takes the first 128 bits, and the seat number takes the last 128 bits
+            uint256 nftId = (uint256(eventId) << 128) | (uint256(events[eventId].nextTicketToSell + i));
+            ticketNFT.mintFromMarketPlace(msg.sender, nftId);
+        }
+        // Update the nextTicketToSell
+        events[eventId].nextTicketToSell += ticketCount;
+
+        emit TicketsBought(eventId, ticketCount, "ETH");
     }
 
     function buyTicketsERC20(uint128 eventId, uint128 ticketCount) external override {
-        // placeholder
+        // no owner check.
+
+        // check for overflow
+        if (ticketCount > (type(uint256).max / events[eventId].pricePerTicketERC20)) {
+            revert("Overflow happened while calculating the total price of tickets. Try buying smaller number of tickets.");
+        }
+
+        // check if the user has enough funds
+        if (IERC20(erc20Address).balanceOf(msg.sender) < (events[eventId].pricePerTicketERC20 * ticketCount)) {
+            revert("Not enough funds supplied to buy the specified number of tickets.");
+        }
+
+        // check if we have enough tickets to sell
+        if (events[eventId].maxTickets < (events[eventId].nextTicketToSell + ticketCount)) {
+            revert("We don't have that many tickets left to sell!");
+        }
+
+        // Transfer the funds
+        IERC20(erc20Address).transferFrom(msg.sender, address(this), (events[eventId].pricePerTicketERC20 * ticketCount));
+
+        // Mint the tickets
+        for (uint128 i = 0; i < ticketCount; i++) {
+            // Same as above
+            uint256 nftId = (uint256(eventId) << 128) | (uint256(events[eventId].nextTicketToSell + i));
+            ticketNFT.mintFromMarketPlace(msg.sender, nftId);
+        }
+        events[eventId].nextTicketToSell += ticketCount;
+
+        emit TicketsBought(eventId, ticketCount, "ERC20");
     }
 }
--- hw/hw3/CSC2125_HW_TicketMarketplace/CSC2125_HW_TicketMarketplace-main/contracts/TicketNFT.sol
+++ hw/hw3/CSC2125_HW_TicketMarketplace/CSC2125_HW_TicketMarketplace-main/contracts/TicketNFT.sol
@@ -11,7 +11,7 @@ contract TicketNFT is ERC1155, ITicketNFT {
     // your code goes here (you can do it!)
     // Reference: https://docs.openzeppelin.com/contracts/5.x/erc1155
     // OpenZeppelin docs | ERC1155
-    
+
     address private _owner;
 
     constructor(address marketplaceOwner) ERC1155("https://api.ticketnft.com/ticket/{id}.json") {
@@ -24,6 +24,9 @@ contract TicketNFT is ERC1155, ITicketNFT {
     }
 
     function mintFromMarketPlace(address to, uint256 nftId) external override {
-        // Intentionally left empty
+        // _mint(to: address, id: uint256, amount: uint256, data: bytes)
+        // Passing an empty string as according to "https://piazza.com/class/lqobnxugstjce/post/19"
+        // Also according to ERC1155: https://docs.openzeppelin.com/contracts/5.x/erc1155#_mint 
+        _mint(to, nftId, 1, "");
     }
 }
